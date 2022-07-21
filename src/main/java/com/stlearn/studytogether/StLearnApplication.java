51a4b1a9ac9910608f98a8eac46284ce8d76b4be
skysmell <skysmeli@kakao.com>
2022-07-21 18:19:55 +0900

    start project

---
+++ src/main/java/com/stlearn/studytogether/StLearnApplication.java
@@ -0,0 +1,13 @@
+package com.stlearn.studytogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stlearn/studytogether/StLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stlearn.studytogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
